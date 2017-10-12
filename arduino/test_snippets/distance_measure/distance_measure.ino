cbcd5a84f88e94e3708bd69b1e76f3b53d414cac
ahemberg <UfQRemBWB6fHxXtHV3Id>
2017-10-12 21:52:09 +0200

    Update

--- arduino/test_snippets/distance_measure/distance_measure.ino
+++ arduino/test_snippets/distance_measure/distance_measure.ino
@@ -15,21 +15,20 @@
  void loop() 
  { 
    digitalWrite(outputPin, HIGH); //Trigger ultrasonic detection 
-   delayMicroseconds(10); 
+   delayMicroseconds(100); 
    digitalWrite(outputPin, LOW); 
    int distance = pulseIn(inputPin, HIGH); //Read ultrasonic reflection
-   distance= distance/58; //Calculate distance 
+   //distance= distance/58; //Calculate distance 
    Serial.print(distance); //Print distance 
    Serial.print(",");
-   delay(10); 
 
    digitalWrite(outputPin2, HIGH); //Trigger ultrasonic detection 
-   delayMicroseconds(10); 
-   digitalWrite(outputPin2, LOW); 
+   delayMicroseconds(100); 
+   digitalWrite(outputPin2, LOW);
    int distance2 = pulseIn(inputPin2, HIGH); //Read ultrasonic reflection
-   distance2= distance2/58; //Calculate distance 
+   //distance2= distance2/58; //Calculate distance 
+   
    Serial.println(distance2); //Print distance 
-   delay(10); 
    
  } 
 
