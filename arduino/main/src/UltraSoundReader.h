40af2cb5404076255660d31cb1eb2a00c3ed0311
Lars Lowe Sjosund <lars@peltarion.com>
2017-10-14 22:30:28 +0200

    Renamed to camelcase

---
+++ arduino/main/src/UltraSoundReader.h
@@ -0,0 +1,14 @@
+/*
+ * Some kind of file explanation
+ */
+
+class UltraSoundReader {
+    private:
+        int trigger, echo, distance;
+    protected:
+
+    public:
+        UltraSoundReader(int trigger_pin, int echo_pin);
+        void read_sensor();
+        long get_distance();
+};
