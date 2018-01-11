9368caf7a74fd52e1aaa92b4640542b86759e747
ahemberg <UfQRemBWB6fHxXtHV3Id>
2018-01-11 21:32:42 +0100

    Moved pin #defines to separate h file

--- arduino/main/src/main.cpp
+++ arduino/main/src/main.cpp
@@ -2,22 +2,8 @@
 #include <UltraSoundReader.h>
 #include <MotorController.h>
 #include <ServoController.h>
+#include <pin_definitions.h>
 
-// Defines for UltraSoundReader
-#define TRIG 11
-#define ECHO  12
-
-// Defines for ServoController
-#define ENABLE_SERVO 8
-#define PWM_SERVO 9
-#define SERVO_AIN1 10
-#define SERVO_AIN2 3
-#define SERVO_MEAS A0
-
-// Defines for MotorController
-#define INPUT1_MOTOR 5
-#define INPUT2_MOTOR 6
-#define ENABLE_MOTOR 4
 
 short int speed = 0, angle = 0, old_angle = 100;
 bool stopped = false;
