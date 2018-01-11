0a36acb788017469722772c9695cead7a6cd466e
ahemberg <UfQRemBWB6fHxXtHV3Id>
2018-01-11 21:32:42 +0100

    Moved pin #defines to separate h file

---
+++ arduino/main/lib/src/pin_definitions.h
@@ -0,0 +1,15 @@
+// Defines for UltraSoundReader
+#define TRIG 11
+#define ECHO  12
+
+// Defines for ServoController
+#define ENABLE_SERVO 8
+#define PWM_SERVO 9
+#define SERVO_AIN1 10
+#define SERVO_AIN2 3
+#define SERVO_MEAS A0
+
+// Defines for MotorController
+#define INPUT1_MOTOR 5
+#define INPUT2_MOTOR 6
+#define ENABLE_MOTOR 4
